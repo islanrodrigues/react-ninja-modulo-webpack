d262eddba2d563f919ee2e6459bdd4889dba05e2
Islan Rodrigues <jose-islan52@hotmail.com>
2019-08-30 14:55:34 -0300

    Configurando a aplicação para usar o React hot loader

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {colors: true}
+
+}).listen(3100, (err) => {
+    if (err) {
+        console.log(err)
+    }
+
+    console.log('Listening on http://localhost:3100')
+})
